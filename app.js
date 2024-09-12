4ebe0b53684020b8b4bcb3817c9302f6c684f1a4
Abidkhan234 <abidshahkhan237@gmail.com>
2024-09-12 20:18:12 +0500

    Chapter no3 and Chapter no4 done

--- app.js
+++ app.js
@@ -55,9 +55,73 @@
 
 //! Chapter no2 end
 
+//! Chapter no3 start
+
+//? Question no1
+// var age = 17;
+// alert("I am "+age+" years old");
+
+//? Question no2
+// var myvisits= 14;
+// alert("You have visited this site "+myvisits+" times");
+
+//? Question no3
+// var birthYear= 2007;
+// document.write("My birth year is ",birthYear);
+
+//? Question no4
+// var visitorName = "Jhon Doe";
+// var productName = " T-shirt(s) ";
+// var Quentity = "ordered 5";
+// var storeName ="XYZ Clothing store.";
+// document.write(visitorName," ",Quentity,productName," on ",storeName);
+//! Chapter no3 end
+
+//! Chapter no4 start
+
+//? Question no1
+// var a,b,c;
+
+//? Question no2
+// var legalNames = "$Name , _Name , Name , Name_123 , Name123";
+// var illegalNames ="@Name , 123Name , Name-123 , 123 , (Name)";
+
+//? Question no3
+// document.write("Rules for naming JS Variables");
+// document.write("<br>");
+// document.write("<br>");
+// document.write("<br>");
+// document.write("<br>");
+// document.write("<br>");
+// document.write("<br>");
+
+// document.write("Variables names can only contain , number , $ and _ . For Example:$any_1stVariable");
+// document.write("<br>");
+// document.write("Variables names must begin with a , $ , _ or letter. For Example: $name, _name or name");
+// document.write("<br>");
+// document.write("Variables name are case sensitive");
+// document.write("<br>");
+// document.write("Variables names should not be JS keywors");
+
+//! Chapter no4 end
+
+//! Chapter no5 start
+
+//? Question no1
+// var num1 =1;
+// var num2 =2;
+// var sum = num1 + num2;
+// document.write("Sum of ",num1," + ",num2," is ",sum);
+
+//? Question no2
 
 
+//? Question no3
+//? Question no4
+//? Question no5
 
+
+//! Chapter no5 end
 
 
 
