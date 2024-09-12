a43025f47bdeaf73d19bc2ef86b1ea6a422bf1bd
Abidkhan234 <abidshahkhan237@gmail.com>
2024-09-12 11:37:32 +0500

    chapter no1 and chapter no2 done

---
+++ app.js
@@ -0,0 +1,63 @@
+//! Chapter no1 start
+
+//? Question no1 
+// alert("Hello Visitor");
+
+//? question no2
+// alert("Error! Please enter a valid password");
+
+//? Question no3
+// alert("Welcome to Js Land... \nHappy Coding!");
+
+//? Question no4
+// alert("Welcome to Js Land...");
+// alert("Happy Coding!");
+
+//? Question no5
+// console.log("Hello... I can run JS through browser's console");
+ 
+//! Chapter no1 end
+
+//! Chapter no2 start
+
+//? Question no1 
+// var username;
+
+//? Question no2
+// var myName="Abid shah khan";
+
+//? Question no3
+// var message= "Hello World";
+// alert(message);
+
+//? Question no4
+// var studentData = "Certified Mobile Application Developer";
+// alert(studentData);
+
+//? Question no5
+// var message ="PIZZA\nPIZZ\nPIZ\nPI\nP";
+// alert(message);
+
+//? Question no6
+// var myEmail="example@example.com";
+// alert("My email address is " + myEmail); 
+
+//? Question no7
+// var message="A smart way to learn JavaScript";
+// alert("I am trying to learn from the Book"+message);
+
+//? Question no8
+// document.write("Yah! I can write HTMl content throught JavaScript");
+
+//? Question no9
+// var myDesign= "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+// alert(myDesign);
+
+//! Chapter no2 end
+
+
+
+
+
+
+
