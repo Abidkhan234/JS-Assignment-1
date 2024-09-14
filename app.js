ae5f75fbbd1e213e6a1134eb54f9b8f74c90a913
Abidkhan234 <abidshahkhan237@gmail.com>
2024-09-14 14:58:36 +0500

    chapter no4 and

--- app.js
+++ app.js
@@ -87,7 +87,7 @@
 // var illegalNames ="@Name , 123Name , Name-123 , 123 , (Name)";
 
 //? Question no3
-// document.write("Rules for naming JS Variables");
+// document.write("<h1>Rules for naming JS Variables</h1>");
 // document.write("<br>");
 // document.write("<br>");
 // document.write("<br>");
@@ -108,18 +108,230 @@
 //! Chapter no5 start
 
 //? Question no1
-// var num1 =1;
+// var num1 =3;
 // var num2 =2;
 // var sum = num1 + num2;
 // document.write("Sum of ",num1," + ",num2," is ",sum);
 
 //? Question no2
+// For Subtaraction
+// var num1 =3;
+// var num2 =2;
+// var sub = num1 - num2;
+// document.write("<br>");
+// document.write("Subtraction of ",num1," + ",num2," is ",sub);
 
+// For multiplication
+// var num1 =3;
+// var num2 =2;
+// var mul = num1 * num2;
+// document.write("<br>");
+// document.write("Multiplication of ",num1," + ",num2," is ",mul);
+
+// For Division
+// var num1 =3;
+// var num2 =2;
+// var div = num1 / num2;
+// document.write("<br>");
+// document.write("Division of ",num1," + ",num2," is ",div);
+
+// For Modulus
+// var num1 =3;
+// var num2 =2;
+// var mod = num1 % num2;
+// document.write("<br>");
+// document.write("Modulus of ",num1," + ",num2," is ",mod);
 
 //? Question no3
-//? Question no4
-//? Question no5
+// var num;
+// document.write("Value after variable declaration is: ",num);
+// document.write("<br>");
+// document.write("<br>");
 
+// num = 5;
+// document.write("Initial value: ",num);
+// document.write("<br>");
+// document.write("<br>");
+
+// num = 5 + 1;
+// document.write("Value after increment is: ",num);
+// document.write("<br>");
+// document.write("<br>");
+
+// num = num + 7;
+// document.write("Value after addition is: ",num);
+// document.write("<br>");
+// document.write("<br>");
+
+// num = num - 1;
+// document.write("Value after decrement is: ",num);
+// document.write("<br>");
+// document.write("<br>");
+
+// num = num % 3 ;
+// document.write("The remainder is: ",num);
+
+//? Question no4
+// var ticket = 600;
+// var totalCost =600 * 5;
+// document.write("Original prize ",ticket);
+// document.write("<br>")
+// document.write("After buying 5 tickets ",totalCost);
+
+//? Question no5
+// var num = 4;
+// document.write("<h1>Tabel of 4</h1>");
+// document.write("4 * 1 = ",num);
+// document.write("<br>");
+// document.write("4 * 2 = ",num*2);
+// document.write("<br>");
+// document.write("4 * 3 = ",num*3);
+// document.write("<br>");
+// document.write("4 * 4 = ",num*4);
+// document.write("<br>");
+// document.write("4 * 5 = ",num*5);
+// document.write("<br>");
+// document.write("4 * 6 = ",num*6);
+// document.write("<br>");
+// document.write("4 * 7 = ",num*7);
+// document.write("<br>");
+// document.write("4 * 8 = ",num*8);
+// document.write("<br>");
+// document.write("4 * 9 = ",num*9);
+// document.write("<br>");
+// document.write("4 * 10 = ",num*10);
+
+//? Question 6
+// var NNoC =25;
+// var NNoF = 70;
+
+// var ConverterOfNNoC = (NNoC*9/5)+32;
+// document.write(NNoC,"<sup>o</sup>C is ",ConverterOfNNoC,"<sup>o</sup>F"); 
+
+// document.write("<br>");
+// document.write("<br>");
+
+// var ConverterOfNNoF =(NNoF-32)*5/9 ;
+// document.write(NNoF,"<sup>o</sup>F is ",ConverterOfNNoF,"<sup>o</sup>C");
+
+//? Question 7
+// var item1 =650;
+// var Quentity1 = 2;
+// document.write("Price of item 1: ",item1);
+
+// document.write("<br>");
+// document.write("<br>");
+
+// document.write("Ordered quantity of item 1: ",Quentity1)
+
+// document.write("<br>");
+// document.write("<br>");
+
+// var item2 = 350;
+// var Quentity2 = 3;
+// document.write("Price of item 2: ",item2);
+
+// document.write("<br>");
+// document.write("<br>");
+
+// document.write("Ordered quantity of item 2: ",Quentity2)
+
+// document.write("<br>");
+// document.write("<br>");
+
+// var shippingCharges = 100;
+
+// var totalCost = (item1*Quentity1) + (item2*Quentity2) + shippingCharges;  
+// document.write("Your total cost is: ",totalCost);
+
+//? Question 8
+// document.write("<h1>Mark Sheet</h1>");
+
+// var totalMarks = 950;
+// document.write("Toatal marks is: ",totalMarks);
+
+// document.write("<br>")
+
+// var obtainedMarks = 550;
+// document.write("obtained marks is: ",obtainedMarks);
+
+// document.write("<br>")
+
+// var percentageOfstudent = obtainedMarks/totalMarks*100;
+// document.write("Student percentage is: ",percentageOfstudent,"%");
+
+//? Question 9
+// document.write("<h1>Currency in PKR</h1>");
+
+// var dollarCurrency = 104.80;
+// var saudiCurrency = 28;
+
+// var totalRupees = (dollarCurrency*10)+(saudiCurrency*25);
+// document.write("Total Currency  in PKR: ",totalRupees);
+
+//? Question 10
+// var num = 5;
+// var arithmeticOperation = ((num+5)*10)/2 ;
+// document.write(arithmeticOperation);
+
+//? Question 11
+// document.write("<h1>Age Calculator</h1>");
+
+// var currentYear = 2024;
+// document.write("Current year: ",currentYear);
+// document.write("<br>")
+// document.write("<br>")
+
+// var birthYear = 2007;
+// document.write("Birth year: ",birthYear);
+// document.write("<br>")
+// document.write("<br>")
+
+// var age = currentYear - birthYear;
+ 
+// document.write("Your age is: ",age);
+
+//? Question 12
+// document.write("<h1>The Geometrizer</h1>");
+
+// var r = 20;
+// document.write("Radius of a circle is ",r);
+// document.write("<br>");
+
+// var pi = 3.142;
+
+// var circumference= 2*pi*r;
+// document.write("The circumference is ",circumference);
+// document.write("<br>");
+
+
+// var areaOfcircle = pi*r**2;
+// document.write("The area of circle is ",areaOfcircle);
+
+//? Question 13
+// document.write("<h1>The Lifetime Supply</h1>");
+
+// var favSnack = "Chips";
+// document.write("Favourite snack is ",favSnack);
+// document.write("<br>");
+
+// var currentAge = 15;
+// document.write("Current age is ",currentAge);
+// document.write("<br>");
+
+// var maximumAge =  65;
+// document.write("Maximum age is ",maximumAge);
+// document.write("<br>");
+
+// var amountPerday = 3;
+// document.write("Amount of snacks per day ",amountPerday);
+// document.write("<br>");
+
+// var remainingYear =  maximumAge - currentAge;
+
+// var totalAmount =remainingYear * amountPerday;
+
+// document.write("You will need ",totalAmount," to last you until the ripe old age of ",maximumAge);
 
 //! Chapter no5 end
 
